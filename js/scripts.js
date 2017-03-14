13934bb85d15e517a768fa7a2d3bb7f8ee6cd6cd
Marilyn Carlin and Micky Matthews <micky@>
2017-03-13 19:47:42 -0700

    add js function to concatonate first and last name to a fullName variable, and reveal objects via click function.

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,9 @@ function Contact(first, last) {
   this.firstName = first;
   this.lastName = last;
 }
-
+Contact.prototype.fullName = function() {
+  return this.firstName + " " + this.lastName;
+}
 
 // user interface logic
 $(document).ready(function() {
@@ -16,6 +18,13 @@ $(document).ready(function() {
     var newContact = new Contact(inputtedFirstName, inputtedLastName);
 
     $("ul#contact").append("<li>span class='contact'>" + newContact.firstName + "</span></li>");
+
+    $(".contact").last().click(function() {
+      $("#show-contact").show();
+      $("#show-contact h2").text(newContact.firstName);
+      $(".first-name").text(newContact.firstName);
+      $(".last-name").text(newContact.lastName);
+    });
 
 
     $("input#new-first-name").val("");
