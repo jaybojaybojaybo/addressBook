47312a6b3c0b4d29037773cc8cbbfe7a1c38fa86
Jasun Feddema and Marilyn Carlin <marilyn@>
2017-03-15 19:26:21 -0700

    refactor jQuery to create a function to reset form fields, created within the business logic and called at the end of the user-interface logic.

--- js/scripts.js
+++ js/scripts.js
@@ -19,6 +19,14 @@ Address.prototype.fullAddress = function() {
   return this.street + ", " + this.city + ", " + this.state;
 };
 
+function resetFields() {
+  $("input#new-first-name").val("");
+  $("input#new-last-name").val("");
+  $("input.new-street").val("");
+  $("input.new-city").val("");
+  $("input.new-state").val("");
+}
+
 // user interface logic
 $(document).ready(function() {
   $("#add-address").click(function() {
@@ -69,11 +77,7 @@ $(document).ready(function() {
       });
     });
 
-    $("input#new-first-name").val("");
-    $("input#new-last-name").val("");
-    $("input.new-street").val("");
-    $("input.new-city").val("");
-    $("input.new-state").val("");
+    resetFields();
 
   });
 });
